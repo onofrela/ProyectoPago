d4cbc668a3a337e32789f891d783c3ab4bf3e377
Leonardo Arroyo Onofre <arroyoonofrel@gmail.com>
2023-10-19 20:34:06 -0600

    Validaciones a tiempo

--- test/window/PruebaVentana.java
+++ test/window/PruebaVentana.java
@@ -6,7 +6,6 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.swing.border.*;
-import javax.swing.plaf.ColorUIResource;
 import test.window.strategy.strategyEfectivo;
 import test.window.strategy.strategyPayPal;
 import test.window.strategy.strategyTarjetaCredito;
@@ -15,10 +14,11 @@ public class PruebaVentana {
 
     private static JButton crearBoton(String text, String imagePath) {
         JButton button = new JButton(text);
+        button.setFont(new Font("Calibri", Font.PLAIN, 16));
 
         try {
             BufferedImage iconImage = ImageIO.read(PruebaVentana.class.getResourceAsStream(imagePath));
-            ImageIcon icon = new ImageIcon(iconImage.getScaledInstance(50, 50, Image.SCALE_SMOOTH));
+            ImageIcon icon = new ImageIcon(iconImage.getScaledInstance(75, 75, Image.SCALE_SMOOTH));
             button.setIcon(icon);
         } catch (IOException e) {
             e.printStackTrace();
@@ -33,11 +33,13 @@ public class PruebaVentana {
 
         return button;
     }
+    
     public static void main(String[] args) {
 
         JFrame frame = new JFrame("Proceso de pago");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
+        frame.setSize(720, 480);
+        frame.setLocationRelativeTo(null);
 
         try {
             BufferedImage iconImage = ImageIO.read(PruebaVentana.class.getResourceAsStream("/test/window/img/icono.png"));
@@ -46,24 +48,41 @@ public class PruebaVentana {
             e.printStackTrace();
         }
 
-        JPanel contentPane = new JPanel(new BorderLayout());
+        JPanel contentPane = new JPanel();
+        GroupLayout layout = new GroupLayout(contentPane);
+        contentPane.setLayout(layout);
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+        
+        JPanel botones = new JPanel(new GridLayout(1, 0, 10, 0)); // 1 fila, 0 columnas, espacio horizontal de 10 píxeles
+
 
         JLabel label = new JLabel("Elige el método para pagar");
-        label.setFont(new Font("Calibri", Font.PLAIN, 16));
+        label.setFont(new Font("Calibri", Font.BOLD, 32));
         label.setHorizontalAlignment(SwingConstants.CENTER);
-        contentPane.add(label, BorderLayout.NORTH);
-
-        JPanel subPanel = new JPanel(new GridLayout(1, 0, 10, 0));
+        contentPane.add(label);
 
         JButton buttonEfectivo = crearBoton("Efectivo", "/test/window/img/efectivo.png");
         JButton buttonPayPal = crearBoton("PayPal", "/test/window/img/paypal.png");
         JButton buttonTarjetaCredito = crearBoton("Tarjeta de Crédito", "/test/window/img/tarjeta.png");
 
-        subPanel.add(buttonEfectivo);
-        subPanel.add(buttonPayPal);
-        subPanel.add(buttonTarjetaCredito);
+        botones.add(buttonEfectivo);
+        botones.add(buttonPayPal);
+        botones.add(buttonTarjetaCredito);
 
-        contentPane.add(subPanel, BorderLayout.CENTER);
+        contentPane.setBorder(new EmptyBorder(50, 50, 50, 50));
+
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.CENTER)
+                .addComponent(label)
+                .addComponent(botones)
+        );
+
+        layout.setVerticalGroup(
+            layout.createSequentialGroup()
+                .addComponent(label)
+                .addComponent(botones)
+        );
 
         frame.setContentPane(contentPane);
         frame.setVisible(true);
@@ -71,49 +90,5 @@ public class PruebaVentana {
         buttonEfectivo.addActionListener(new strategyEfectivo(frame));
         buttonPayPal.addActionListener(new strategyPayPal(frame));
         buttonTarjetaCredito.addActionListener(new strategyTarjetaCredito(frame));
-    }
-}
-
-import java.awt.*;
-import javax.swing.*;
-import javax.swing.border.*;
-
-public class Main {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Prueba");
-        frame.setSize(400, 200);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        
-        JPanel principal = new JPanel();
-        GroupLayout layout = new GroupLayout(principal);
-        principal.setLayout(layout);
-        layout.setAutoCreateGaps(true);
-        layout.setAutoCreateContainerGaps(true);
-        
-        JPanel cards = new JPanel(new GridLayout(1, 0, 10, 0)); // 1 fila, 0 columnas, espacio horizontal de 10 píxeles
-        JButton boton1 = new JButton("Haz click");
-        JButton boton2 = new JButton("Haz click");
-        cards.add(boton1);
-        cards.add(boton2);
-    
-        JLabel texto = new JLabel("Ingrese el método de pago");
-
-        principal.setBorder(new EmptyBorder(50, 50, 50, 50));
-
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(GroupLayout.Alignment.CENTER)
-                .addComponent(texto)
-                .addComponent(cards)
-        );
-
-        layout.setVerticalGroup(
-            layout.createSequentialGroup()
-                .addComponent(texto)
-                .addComponent(cards)
-        );
-
-        frame.add(principal);
-        frame.setVisible(true);
     }
 }
